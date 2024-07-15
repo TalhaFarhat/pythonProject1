8b8c56ac4bee72774b12b8beeb0d0e1b8f0f5987
TalhaFarhat <talhafarhat02@gmail.com>
2024-07-15 02:18:34 -0400

    new feature implemented

--- gui.py
+++ gui.py
@@ -8,6 +8,19 @@ add_button = sg.Button("Add")
 window = sg.Window('My To-Do App',
                     layout=[[label], [input_box, add_button]],
                     font=('Helvetica, 20'))
-event, values= window.read()
-print(event, values)
+
+while True:
+    event, values= window.read()
+    print(event)
+    print(values)
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
 window.close()
